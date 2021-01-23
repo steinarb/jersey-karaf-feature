9e44c23343040236d1031352b78117df6cb0ae99
Steinar Bang <sb@dod.no>
2021-01-23 11:43:38 +0100

    Add pax exam integration test loading the jersey karaf feature

---
+++ jersey.tests/src/test/java/no/priv/bang/karaf/jersey/tests/JerseyFeatureIntegrationTest.java
@@ -0,0 +1,36 @@
+package no.priv.bang.karaf.jersey.tests;
+
+import static org.ops4j.pax.exam.CoreOptions.*;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
+
+import java.util.stream.Stream;
+
+import org.apache.karaf.itests.KarafTestSupport;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class JerseyFeatureIntegrationTest extends KarafTestSupport {
+
+    @Configuration
+    public Option[] config() {
+        final MavenArtifactUrlReference handleregFeatureRepo = maven().groupId("no.priv.bang.karaf").artifactId("jersey").version("LATEST").type("xml").classifier("features");
+        Option[] options = new Option[] {
+            features(handleregFeatureRepo)
+        };
+        return Stream.of(super.config(), options).flatMap(Stream::of).toArray(Option[]::new);
+    }
+
+    @Test
+    public void testLoadFeature() throws Exception { // NOSONAR this test has an assert, just not an assert sonar recognizes
+        installAndAssertFeature("jersey-karaf-feature");
+    }
+
+}
